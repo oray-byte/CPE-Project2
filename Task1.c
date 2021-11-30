b255e66b9ea88b827c8ce5e0d86a82c1da65a95d
Garrett <47675187+elloGarrett@users.noreply.github.com>
2021-11-30 13:45:51 -0600

    Add files via upload

--- Task1.c
+++ Task1.c
@@ -1,29 +1,31 @@
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <util/delay.h>
+#include "avr/io.h"
+#include "avr/interrupt.h"
 
 					//R0    G0    B0,   R1    G1    B1,   R2    G2    B2,   R3    G3    B3,   R4    G4    B4,   R5    G5    B5,   R6    G6    B6,   R7    G7    B7,   R8    G8    B8,   R9    G9    B9
-int initValues[] = {0x30, 0x00, 0x00, 0x30, 0x10, 0x00, 0x20, 0x20, 0x00, 0x10, 0x30, 0x00, 0x00, 0x30, 0x10, 0x00, 0x20, 0x20, 0x00, 0x10, 0x30, 0x00, 0x00, 0x30, 0x10, 0x00, 0x20, 0x20, 0x00, 0x10};
-int neoPixels[30];
+unsigned char initValues[] = {0x30, 0x00, 0x00, 0x30, 0x10, 0x00, 0x20, 0x20, 0x00, 0x10, 0x30, 0x00, 0x00, 0x30, 0x10, 0x00, 0x20, 0x20, 0x00, 0x10, 0x30, 0x00, 0x00, 0x30, 0x10, 0x00, 0x20, 0x20, 0x00, 0x10};
+unsigned char neoPixels[30];
 
-
-void clearPixels();
 
 void updatePixels();
 
-void sendPixels(int colors[]);
+void sendPixels(unsigned char R, unsigned char G, unsigned char B);
 
 void initNeo();
 
-void byteLoop(int colorVal);
+void clearPixels();
 
-void delay();
+void byteLoop(unsigned char colorVal);
+
+void sendZero();
+
+void sendOne();
+
 
 int main(){
 	DDRB = 0x00;
 	PORTB = 0;
 	
-	
+	initNeo();
 	
 	return 0;
 }
@@ -33,7 +35,7 @@ void initNeo(){
 		neoPixels[i] = initValues[i]; // Mirrors functionality of assembly
 	}
 	
-	sendPixels(neoPixels);
+	// sendPixels(neoPixels);
 	
 }
 
@@ -43,23 +45,71 @@ void clearPixels(){
 	}
 }
 
-void sendPixels(int colors[]){
-	for(int i = 0; i < 30; i++){
-		byteLoop(colors[i]);
+void updatePixels(){
+	
+	for(int i = 0; i < 10; i++){
+		unsigned char R = neoPixels[3*i]; //Red
+		unsigned char G = neoPixels[3*i + 1]; //Green
+		unsigned char B = neoPixels[3*i + 2]; //Blue
+		
+		sendPixels(R, G, B);
 	}
 }
 
-void byteLoop(int colorVal){
+void sendPixels(unsigned char R, unsigned char G, unsigned char B){
+	byteLoop(R);
+	byteLoop(G);
+	byteLoop(B);
+}
+
+void byteLoop(unsigned char colorVal){
 	//bin = //convert colorVal to binary 
 	
 	for(int i = 0; i < 8; i++){
-		//PORTB.0 = bin[i];
+		PORTB |= 0b00000001; 
+		
+		if((colorVal<<i) == 1)
+		{
+			sendOne();	
+		}
+		else
+		{
+			sendZero();
+		}
+
 	}
 }
 
 
-// countVal is typically 210
-void delay(){
-	_delay_ms(210);
+void sendOne()
+{
+	PORTB |= 0b00000001;
+	
+	// Put 14 clock cycle high timer here
+	TCNT0 = -14;
+	TCCR0A = 0x01;
+	
+	while((TIFR0 & (1 << TOV0)) == 0);
+	
+	TCCR0A = 0;
+	TIFR0 = 0x1; // Clear the timer
+
 }
 
+void sendZero()
+{
+	PORTB &= 0b00000000;	
+	
+	// Put 7 clock cycle low timer here
+	TCNT0 = -7;
+	TCCR0A = 0x01;
+	
+	while((TIFR0 & (1 << TOV0)) == 0);
+	
+	TCCR0A = 0;
+	TIFR0 = 0x1;
+}
+
+/*void delay(){
+	
+}*/
