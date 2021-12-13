e42327412bca64706988a793eb2d1ba89df7771e
oray-byte <56564940+oray-byte@users.noreply.github.com>
2021-12-12 23:04:08 -0600

    Fixed main()
    Added comments
    Added stable-ish button inputs
    Fixed strobe and light after 2 seconds

--- Task2.c
+++ Task2.c
@@ -5,69 +5,107 @@
 unsigned char initValues[] = {0x30, 0x00, 0x00, 0x30, 0x10, 0x00, 0x20, 0x20, 0x00, 0x10, 0x30, 0x00, 0x00, 0x30, 0x10, 0x00, 0x20, 0x20, 0x00, 0x10, 0x30, 0x00, 0x00, 0x30, 0x10, 0x00, 0x20, 0x20, 0x00, 0x10};
 unsigned char neoPixels[30];
 
-
+/*
+ * -Iterates through neoPixels array
+ * -Calls sendPixels()
+ */
 void updatePixels();
 
+/*
+ * -Sends pixels values to pixels
+ * -Calls nextBit()
+ */
 void sendPixels(unsigned char R, unsigned char G, unsigned char B);
 
+/*
+ * -Initialize neopixel values with values in initValues array
+ */
 void initNeo();
 
+/*
+ * -Empty values in neopixels
+ * -Clears neopixels
+ */
 void clearPixels();
 
-void sendZero();
-
-void sendOne();
-
+/*
+ * -Creates strobe light by calling allBlue(), allGreen(), and allRed() while clearing in between
+ */
 void strobe();
 
-void startTimer();
-
-void stopTimer();
-
+/*
+ * -Plays a sound at 1 kHz
+ */
 void emitSound();
 
+/*
+ * -Helps emitSound() create a 500 us square wave
+ */
 void soundDelay();
 
+/*
+ * -Handles button presses.
+ * -Returns 1 if pressed
+ * -Returns 0 if not pressed
+ */
 int buttonPush();
 
-int main() {
+/*
+ * -Delay that helps creates stable button presses
+ * -Uses timer1 to not interfere with timer0
+ * -length: Control how long the delay is
+ */
+void buttonDelay(int length);
+
+int main(void) {
 	DDRB |= (1<<0);
 	PORTB &= 0;
-	DDRD &= 0b11101111; //Button on PD4
-	PORTD |= 0b00010000;
-	DDRC |= 0b01000000; //Speaker on PC6
-	int pressed = 0;
-	
-	initNeo();
+	DDRD &= 0b11101111; // Button on PD4
+	DDRC |= 0b10000000; // Delete
+	PORTD |= 0b00010000; // Pull-up resister
+	DDRC |= 0b01000000; // Speaker on PC6
+	int timer = 0;
+
+	clearPixels(); // If pixels were not cleared before flashing
 	
 	while(1) {
-		if (buttonPush()) {
-			int dimVal = 4;
-			int _i;
-			for (_i = 0; _i < 20; _i++) {
-				partDim(dimVal);
-				
-				dimVal += 4;
-				for (int _j = 0; _j < 10; _j++) {
-					TCNT0 = 0xFE;
-					TCCR0A = 0x00;
-					TCCR0B = 0x05;
-					while ((TIFR0 & (1<<TOV0)) == 0) {
-						if (buttonPush()) {
-							pressed = 1;
-							break;
-						}
-					}
-				}
-				if (pressed)
-				{
-					pressed = 0;
-					break;
-				}
+		int dimVal = 4;
+		// 100ms delay x 20 = 2s delay
+		for (int _i = 0; _i < 20; _i++) {
+			partDim(dimVal); // Responsible for increasing white brightness
+			
+			if (dimVal < 64) // Ensure value does not become greater than 64
+			{
+				dimVal += 3;
 			}
+			
+			for (int _j = 0; _j < 10; _j++) {
+				TCNT0 = 0xB1; // 10 ms delay for 10 times = 100ms delay
+				TCCR0A = 0x00;
+				TCCR0B = 0x05; // Prescaler 1024
+				while ((TIFR0 & (1<<TOV0)) == 0)
+				{
+					timer = TCNT0; // Needed to 'pause' timer
+					if (buttonPush()) // One beep to stop
+					{
+						while (buttonPush() != 1) // Two beeps to start
+						{
+							buttonDelay(0x00); // Needed to create stable button-pressing
+						}
+						TIFR0 = 0x01;
+						TCNT0 = timer; // Needed to 'resume' timer
+					}
+					
+				}
+				TIFR0 = 0x01;
+			}
+		}
 
-			while (buttonPush() != 1 && (_i == 20)) {
-				strobe();
+		// Reached after ~2 seconds
+		while (buttonPush() != 1) { 
+			strobe();
+			for (int i = 0; i < 100; i++) // To waste time
+			{
 				emitSound();
 			}
 		}
@@ -76,45 +114,49 @@ int main() {
 	return 0;
 }
 
-int buttonPush() {
+void buttonDelay(int length)
+{
+	TCNT1H = 0x00;
+	TCNT1L = length;
+	TCCR1A = 0x00;
+	TCCR1B = 0x02;
+	while ((TIFR1 & (1<<TOV1)) == 0);
+	TIFR1 = 0x01;
+}
 
+int buttonPush() {
 	if (PIND & (1<<4)) {
-		emitSound();
+		TCNT1H = 0x00;
+		TCNT1L = 0x91;
+		TCCR1A = 0x00;
+		TCCR1B = 0x02;
+		
+		while ((TIFR1 & (1<<TOV1)) == 0) // Need this so the sound is played long enough to hear
+		{
+			emitSound();
+		}
+		TIFR1 = 0x01;
+		
+		buttonDelay(0xFD); // Make a more stable button input
+		
 		return 1;
 	}
 	else {
 		return 0;
 	}
 }
-void startTimer() {
-	PORTB &= 0b00000000;
-	
-	// Put 7 clock cycle low timer here
-	TCNT0 = -7;
-	TCCR0A = 0x00;
-	TCCR0B = 0x03;
-	
-	while((TIFR0 & (1 << TOV0)) == 0);
-	
-	TCCR0A = 0;
-	TIFR0 = 0x1;
-}
-
-void stopTimer() {
-	
-}
 
 void emitSound() {
-	PORTC |= 0b01000000;
+	PORTC ^= 0b01000000; // Toggle PORTC.6
 	soundDelay();
-	PORTC &= 0b00000000;
+	PORTC ^= 0b01000000; // Toggle PORTC.6
 	soundDelay();
 }
 
 void soundDelay() {
-	TCNT0 = 0xC1;
+	TCNT0 = 0xC1; // 500us delay
 	TCCR0A = 0x00;
-	TCCR0B = 0x03;
+	TCCR0B = 0x03; // Prescaler 64
 
 	while ((TIFR0 & (1<<TOV0)) == 0);
 	TIFR0 = 0x01;
@@ -122,6 +164,7 @@ void soundDelay() {
 
 void strobe() {
 	allRed();
+	updatePixels();
 	delay();
 	clearPixels();
 	delay();
@@ -136,21 +179,13 @@ void strobe() {
 }
 
 void delay() {
-	PORTB = 0x01;
-	TCNT0 = -250;
+	TCNT0 = -500;
 	
 	TCCR0A = 0x00;
 	TCCR0B = 0x05;
 	
-	while((TIFR0 & (1<<TOV0)) == 0); // Wait until overflow flag is set (14 clock cycles)
+	while((TIFR0 & (1<<TOV0)) == 0);
 	
-	PORTB = 0x00; // Set PORTb low
-	
-	// Initialize the timer in normal mode with no prescaling
-	TCCR0A = 0x00;
-	TCCR0B = 0x01;
-	
-	TIFR0 = 1 << TOV0;
 }
 
 void initNeo() {
@@ -232,21 +267,6 @@ void nextBit(unsigned char color) {
 	
 }
 
-void sendOne() {
-	PORTB |= 0b00000001;
-	
-	// Put 14 clock cycle high timer here
-	TCNT0 = -14;
-	TCCR0A = 0x00;
-	TCCR0B = 0x01;
-	
-	while((TIFR0 & (1 << TOV0)) == 0);
-	
-	TCCR0A = 0;
-	TIFR0 = 0x1; // Clear the timer
-
-}
-
 void partDim(int x) {
 	for(int i = 0; i < 10; i++){
 		sendPixels(x, x, x);
@@ -258,30 +278,19 @@ void allGreen() {
 	for(int i = 0; i < 10; i++){
 		sendPixels(64, 0, 0);
 	}
+	updatePixels();
 }
 
 void allRed() {
 	for(int i = 0; i < 10; i++){
 		sendPixels(0, 64, 0);
 	}
+	updatePixels();
 }
 
 void allBlue() {
 	for(int i = 0; i < 10; i++){
 		sendPixels(0,0,64);
 	}
-}
-
-void sendZero() {
-	PORTB &= 0b00000000;
-	
-	// Put 7 clock cycle low timer here
-	TCNT0 = -7;
-	TCCR0A = 0x00;
-	TCCR0B = 0x01;
-	
-	while((TIFR0 & (1 << TOV0)) == 0);
-	
-	TCCR0A = 0;
-	TIFR0 = 0x1;
+	updatePixels();
 }
